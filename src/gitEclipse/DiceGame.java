9c55c93d38d816ad7ad761ec555738fcf684c363
Roskar333 <oskar.heinrich.nowak@gmail.com>
2023-10-10 02:51:32 +0300

    Program basics now work
    By editing the integers and how they're read, the program now prints out the victory message when any of the random rolls are 7.

--- src/gitEclipse/DiceGame.java
+++ src/gitEclipse/DiceGame.java
@@ -8,18 +8,25 @@ public class DiceGame {
 		// TODO Auto-generated method stub
 		
 		
-		
+	
 		Random rand = new Random();
 		
-		for(int i =0; i < 3; i++)
-		{
-		    int answer = rand.nextInt(10) + 1;
-		    System.out.println(answer);
-		    if (answer>=7) {
-			System.out.println("You win!");
-			}
-		}
-	
+		int dice1, dice2, dice3;
+		int i = 1, roll = 0;
+		i++;
+		
+		    dice1= rand.nextInt(10) + 1;
+		    dice2= rand.nextInt(10) + 1;
+		    dice3= rand.nextInt(10) + 1;
+		    
+		   
+		    
+		    System.out.println(dice1);
+		    System.out.println(dice2);
+		    System.out.println(dice3);
+		    if (dice1==7||dice2==7||dice3==7)
+		    	System.out.println("You win!");
+		    
+			
 	}
-
 }
