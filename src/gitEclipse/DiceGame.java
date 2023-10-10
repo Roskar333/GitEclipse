b56d2565b96e333f23d81eed2b140d17900ee521
Roskar333 <oskar.heinrich.nowak@gmail.com>
2023-10-10 03:20:59 +0300

    Added a simple money system.
    The user can input a value for their money. The game plays itself automatically, until the player either loses all their cash, or wins the jackpot by rolling 777.

--- src/gitEclipse/DiceGame.java
+++ src/gitEclipse/DiceGame.java
@@ -10,23 +10,42 @@ public class DiceGame {
 		
 	
 		Random rand = new Random();
+		Scanner in = new Scanner(System.in);
 		
+		int money;
 		int dice1, dice2, dice3;
 		int i = 1, roll = 0;
-		i++;
 		
+		System.out.println("How much money do you have?");
+		money = Integer.parseInt(in.nextLine());
+		
+		while( money>0) {	
+			i++;
 		    dice1= rand.nextInt(10) + 1;
 		    dice2= rand.nextInt(10) + 1;
 		    dice3= rand.nextInt(10) + 1;
-		    
-		   
-		    
+		     
 		    System.out.println(dice1);
 		    System.out.println(dice2);
 		    System.out.println(dice3);
-		    if (dice1==7||dice2==7||dice3==7)
+		    if (dice1==7||dice2==7||dice3==7) {
 		    	System.out.println("You win!");
-		    
-			
+		    	money+=7;
+		    }
+		    else if(dice1==7 && dice2==7 && dice3==7) {
+		    	System.out.println("Jackpot!!!");
+		    	money+=777;
+		    	System.out.println("Your treasure: "+money);
+		    	break;
+		    }
+		    else {
+		    	System.out.println("Lost some money...");
+		    	money-=7;
+		    }
+
+		}
+		if (money<0) {
+			System.out.println("You lost it all.");
+		}
 	}
 }
